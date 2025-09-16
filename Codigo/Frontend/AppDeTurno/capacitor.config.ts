db9c33d2b0a07e078cfc6dcfb164072da0c6909b
Maria <mariapazcgh0@gmail.com>
2025-09-16 18:20:30 -0300

    copia del proyecto grupal

---
+++ Codigo/Frontend/AppDeTurno/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppDeTurno',
+  webDir: 'www'
+};
+
+export default config;
