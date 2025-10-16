0e887f67449295ea65db1944a3a60a8d77b24ae0
Maria <mariapazcgh0@gmail.com>
2025-10-15 21:30:04 -0300

    tab - capture

--- Codigo/Frontend/appdeturnos/src/app/tabs/capture/capture.page.spec.ts
+++ Codigo/Frontend/appdeturnos/src/app/tabs/capture/capture.page.spec.ts
@@ -1,15 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import { CapturePage } from './capture.page';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 describe('CapturePage', () => {
   let component: CapturePage;
   let fixture: ComponentFixture<CapturePage>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), CommonModule, FormsModule],
+      declarations: [CapturePage]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CapturePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- Documentacion/README.md
+++ Documentacion/README.md
@@ -3,4 +3,5 @@ Copia de repositorio grupal. La apps de turnos
 1- hice el documento con la informacion de la app
 2- hice en congunto con mi grupo el DER
 3- realice la coneccion de supabase en mi repositorio personal y ayude en el grupal 
-4- Arme el la tab health completa 
+4- Modifique el diseño del login 
+5- arme la tab de capture 
